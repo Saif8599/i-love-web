3b01d227e67c162e70bf03e581d81339f9411923
Saif8599 <78470780+Saif8599@users.noreply.github.com>
2025-04-14 00:10:56 +0200

    feat: dynamic site with express & liquid

--- script/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let bookCover = document.querySelector(".book-cover");
-
-  bookCover.addEventListener("click", () => {
-    bookCover.classList.toggle("visible");
-  });
-});
