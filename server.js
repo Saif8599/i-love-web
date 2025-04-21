ab5490574cf22414e803dcb5a21da2bff7a3210e
Saif8599 <78470780+Saif8599@users.noreply.github.com>
2025-04-21 21:39:40 +0200

    feat: layout-shift example #10

--- server.js
+++ server.js
@@ -1,58 +1,68 @@
 // Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
 // Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
-import express from 'express'
+import express from "express";
 
 // Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
-import { Liquid } from 'liquidjs';
+import { Liquid } from "liquidjs";
 
 // Zodat we bestanden en mappen in kunnen lezen.. Cool!
-import { readdir, readFile } from 'node:fs/promises'
+import { readdir, readFile } from "node:fs/promises";
 
 // Markdown parsen, ook cool!
-import { marked } from 'marked'
+import { marked } from "marked";
 
 // Frontmatter uitlezen, nog cooler!
-import matter from 'gray-matter'
+import matter from "gray-matter";
 
 // Lees alle bestandsnamen uit de content directory in
-const files = await readdir('content')
+const files = await readdir("content");
 
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
-const app = express()
+const app = express();
 
 // Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
 // Bestanden in deze map kunnen dus door de browser gebruikt worden
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Stel Liquid in als 'view engine'
 const engine = new Liquid();
-app.engine('liquid', engine.express()); 
+app.engine("liquid", engine.express());
 
 // Stel de map met Liquid templates in
 // Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
-app.set('views', './views')
+app.set("views", "./views");
 
-app.get('/', async function(request, response) {
-  response.render('home.liquid')
-})
+app.get("/", async function (request, response) {
+  response.render("home.liquid");
+});
 
-app.get('/learning-journal', async function(req, res) {
-  res.render('learning-journal.liquid', {files: files})
-})
+app.get("/learning-journal", async function (req, res) {
+  res.render("learning-journal.liquid", { files: files });
+});
 
-app.get('/learning-journal/:slug', async function(req, res) {
-  const fileContents = await readFile('content/' + req.params.slug + '.md', { encoding: 'utf8' })
-  const article = matter(fileContents)
-  const markedUpContent = marked.parse(article.content)
+app.get("/learning-journal/:slug", async function (req, res) {
+  const fileContents = await readFile("content/" + req.params.slug + ".md", {
+    encoding: "utf8",
+  });
+  const article = matter(fileContents);
+  const markedUpContent = marked.parse(article.content);
 
-  res.render('artikel.liquid', {fileContents: markedUpContent, metadata: article.data})
-})
+  res.render("artikel.liquid", {
+    fileContents: markedUpContent,
+    metadata: article.data,
+  });
+});
+
+app.get("/playground", async function (request, response) {
+  response.render("playground.liquid");
+});
+
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
-app.set('port', process.env.PORT || 8000)
+app.set("port", process.env.PORT || 8000);
 
 // Start Express op, haal daarbij het zojuist ingestelde poortnummer op
-app.listen(app.get('port'), function () {
+app.listen(app.get("port"), function () {
   // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
